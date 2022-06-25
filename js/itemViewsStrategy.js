2144d42e3acf2ab01afb9ee06f62c48ffd738510
Fernando Hernandez <fernandohruz@hotmail.com>
2022-06-24 20:16:30 -0500

    feat: cart items are generated dynamically

--- js/itemViewsStrategy.js
+++ js/itemViewsStrategy.js
@@ -1,24 +1,23 @@
 export class InfoContext {
-    constructor(strategy, data, element) {
-        this.setStrategy(strategy);
-        this.data = data;
-        this.element = element;
-    }
-    setStrategy(strategy) {
-        this.strategy = strategy;
-    }
-    show() {
-        this.strategy.show(this.data, this.element);
-    }
+  constructor(strategy, data, element) {
+    this.setStrategy(strategy);
+    this.data = data;
+    this.element = element;
+  }
+  setStrategy(strategy) {
+    this.strategy = strategy;
+  }
+  show() {
+    this.strategy.show(this.data, this.element);
+  }
 }
 
-
 export class CatalogueStrategy {
-    show(data, element) {
-        element.innerHTML = data.reduce((prevItem, item) => {
-            return (
-                prevItem +
-                `<div class="row mb-5">
+  show(data, element) {
+    element.innerHTML = data.reduce((prevItem, item) => {
+      return (
+        prevItem +
+        `<div class="row mb-5">
                 <div class="col-lg-6">
                     <img src="${item.img}" class="card-img image change">
                 </div>    
@@ -32,18 +31,17 @@ export class CatalogueStrategy {
                    
                 </div>
                 `
-            );
-        }, "");
-    }
+      );
+    }, "");
+  }
 }
 
-
 export class ProductViewStrategy {
-    show(data, element) {
-        element.innerHTML = data.reduce((prevItem, item) => {
-            return (
-                prevItem +
-                `<div class="card ">
+  show(data, element) {
+    element.innerHTML = data.reduce((prevItem, item) => {
+      return (
+        prevItem +
+        `<div class="card ">
                     <img src="${item.img}" class="card-img">
                     <div class="card-body">
                         <h5 class="card-title">${item.name}</h5>
@@ -53,24 +51,47 @@ export class ProductViewStrategy {
                         <button class="btn btn-link btn-sm float-right">Añadir a carrito</button>
                     </div>
                 </div>`
-            );
-        }, "");
-    }
+      );
+    }, "");
+  }
 }
 
-
 export class ShoppingCartStrategy {
-    show(data, element) {
-        element.innerHTML = data.reduce((prevItem, item) => {
-            return (
-                prevItem +
-                `<div>
-                    <h4>${item.name} </h4>
-                    <p>${item.cost}</p>
-                    <p>${item.detail}</p>
-                    <p>${item.pieces}</p>
-                </div>`
-            );
-        }, "");
-    }
+  show(data, element) {
+    element.innerHTML = data.reduce((prevItem, item) => {
+      return (
+        prevItem +
+        `
+                 <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <img src="../assets/cataloguePics/${
+                        item.image
+                      }" alt="" width="40px"> 
+                      <div>
+                        <h6 class="my-0">${item.name}</h6>
+                        <small class="text-muted">Número de piezas: ${
+                          item.pieces
+                        }</small>
+                      </div>
+                      <img src="../assets/cart2.svg"></img>
+                      <div>
+                        <span style="display: block;"class="text-muted my-0">$${
+                          item.cost
+                        }</span>
+                        <span>$${item.cost * item.pieces}</span>
+                      </div>
+                    </li>
+                `
+      );
+    }, "");
+    const precioTotal = data.reduce((prevItem, item) => {
+      return prevItem + item.cost * item.pieces;
+    }, 0);
+    console.log(precioTotal);
+    element.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between">
+                      <span>Total (MXN)</span>
+                      <strong>$${precioTotal}</strong>
+                    </li>
+    `;
+  }
 }
--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -1,2 +1,21 @@
 import { header } from "./header.js";
+import { InfoContext, ShoppingCartStrategy } from "./itemViewsStrategy.js";
 petiteHeader.innerHTML = header;
+
+const shoppingCartData = [
+  { image: "3.cama.jpg", name: "Cama", pieces: "3", cost: 230 },
+  { image: "6.pechera_perro.jpg", name: "Pechera", pieces: "1", cost: 320 },
+  {
+    image: "2.plato_de_comida.jpg",
+    name: "Plato de comida",
+    pieces: "2",
+    cost: 130,
+  },
+];
+
+let cartElements = new InfoContext(
+  new ShoppingCartStrategy(),
+  shoppingCartData,
+  shoppingCartList
+);
+cartElements.show();
