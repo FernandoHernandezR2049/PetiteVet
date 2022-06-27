5449482a600812fc29d79d6f7ed7756e9c33e4a2
ferfar92 <ffarias934@gmail.com>
2022-06-27 01:12:53 -0500

    Fix: Changed the relative paths of the pages. close #11

--- js/header.js
+++ js/header.js
@@ -1,10 +1,10 @@
 export const header = `
 <a
   class="navbar-brand"
-  href="./../index.html"
+  href="/index.html"
   style="font-family: 'Lobster'; color: #d93073; font-size: 2rem"
   ><img
-    src="./../assets/logo.png"
+    src="/assets/logo.png"
     alt="Petite Vet Logo"
     width="50px"
     height="33px"
@@ -26,21 +26,21 @@ export const header = `
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto mr-auto">
     <li class="nav-item">
-      <a class="nav-link" href="./../index.html">Home </a>
+      <a class="nav-link" href="/index.html">Home </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="./pages/catalogue.html">Catálogo</a>
+      <a class="nav-link" href="/pages/catalogue.html">Catálogo</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link active" href="./pages/services.html"
+      <a class="nav-link active" href="/pages/services.html"
         >Servicios <span class="sr-only">(current)</span></a
       >
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="./pages/contact.html">Contacto</a>
+      <a class="nav-link" href="/pages/contact.html">Contacto</a>
     </li>
     <li class="nav-item">
-    <a class="nav-link" href="./pages/blog.html">Blog</a>
+    <a class="nav-link" href="/pages/blog.html">Blog</a>
   </li>
     <li class="nav-item dropdown">
       <a
@@ -54,10 +54,10 @@ export const header = `
         Sobre nosotros
       </a>
       <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="./pages/aboutUs.html"
+        <a class="dropdown-item" href="/pages/aboutUs.html"
           >Equipo desarrollador (Vet Devs)</a
         >
-        <a class="dropdown-item" href="./pages/about_petite_vet.html"
+        <a class="dropdown-item" href="/pages/about_petite_vet.html"
           >Veterinaria Petit Vet</a
         >
       </div>
@@ -65,16 +65,16 @@ export const header = `
   </ul>
   <ul class="navbar-nav my-2 my-lg-0">
     <li class="nav-item my-2 my-lg-0">
-      <a class="nav-link" href="./pages/shopping_cart.html"
+      <a class="nav-link" href="/pages/shopping_cart.html"
         ><img
           class="my-icon"
-          src="./../assets/cart2.svg"
+          src="/assets/cart2.svg"
           alt=""
           srcset=""
       /></a>
     </li>
     <li class="nav-item">
-      <a class="btn btn-pink" href="./access.html">Acceso</a>
+      <a class="btn btn-pink" href="/access.html">Acceso</a>
     </li>
   </ul>
 </div>
